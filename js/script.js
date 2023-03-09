184b23fa32a17edf8569718338f54570e7d267b1
akachi11 <118525443+akachi11@users.noreply.github.com>
2023-03-09 23:35:16 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -146,11 +146,12 @@ dropdowns.forEach(dropdown => {
   const select = dropdown.querySelector('.select');
   const caret = dropdown.querySelector('.caret');
   const menu = dropdown.querySelector('.menu');
+  const options = dropdown.querySelectorAll('.menu li')
   const selected = dropdown.querySelector('.selected');
 
   select.addEventListener('click', () => {
     select.classList.toggle('select-clicked');
-    select.classList.toggle('caret-rotate');
+    caret.classList.toggle('caret-rotate');
     menu.classList.toggle('menu-open');
   });
 
